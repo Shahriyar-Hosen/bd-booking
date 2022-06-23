2cab2235a1364b8f385a414453209f42779f48c9
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-23 15:38:58 +0600

    Home Hotel & List Routes add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home";
+import Hotel from "./Pages/Hotel/Hotel";
+import List from "./Pages/List/List";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/hotels" element={<List />}></Route>
+        <Route path="/hotels/:id" element={<Hotel />}></Route>
+      </Routes>
       <h1>Hello Hotel</h1>
     </div>
   );

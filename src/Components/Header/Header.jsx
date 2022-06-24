6676dbcbdfe1e6bcfb170d6d3919ef806e5fc5ed
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-24 16:08:56 +0600

    All icon add done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { MdOutlineHotel,MdLocalTaxi } from "react-icons/md";
+import { IoIosAirplane } from "react-icons/io";
+import { AiFillCar } from "react-icons/ai";
+import { RiHotelBedLine } from "react-icons/ri";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="headerList">
-        <div className="headerListItem"></div>
+        <div className="headerListItem">
+          <MdOutlineHotel />
+          Stays
+        </div>
+        <div className="headerListItem">
+          <IoIosAirplane />
+          Flights
+        </div>
+        <div className="headerListItem">
+          <AiFillCar />
+          Car Rentals
+        </div>
+        <div className="headerListItem">
+          <RiHotelBedLine />
+          Attractions
+        </div>
+        <div className="headerListItem">
+          <MdLocalTaxi />
+          Airport taxis
+        </div>
       </div>
     </div>
   );

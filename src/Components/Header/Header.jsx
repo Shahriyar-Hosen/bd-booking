ed14fa989de7f7fb5e563c4f2c665caccfec9d09
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-24 16:18:19 +0600

    icon add & style done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdOutlineHotel,MdLocalTaxi } from "react-icons/md";
+import { MdOutlineHotel, MdLocalTaxi } from "react-icons/md";
 import { IoIosAirplane } from "react-icons/io";
 import { AiFillCar } from "react-icons/ai";
 import { RiHotelBedLine } from "react-icons/ri";
@@ -8,26 +8,28 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-          <MdOutlineHotel />
-          Stays
-        </div>
-        <div className="headerListItem">
-          <IoIosAirplane />
-          Flights
-        </div>
-        <div className="headerListItem">
-          <AiFillCar />
-          Car Rentals
-        </div>
-        <div className="headerListItem">
-          <RiHotelBedLine />
-          Attractions
-        </div>
-        <div className="headerListItem">
-          <MdLocalTaxi />
-          Airport taxis
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <MdOutlineHotel />
+            Stays
+          </div>
+          <div className="headerListItem">
+            <IoIosAirplane />
+            Flights
+          </div>
+          <div className="headerListItem">
+            <AiFillCar />
+            Car Rentals
+          </div>
+          <div className="headerListItem">
+            <RiHotelBedLine />
+            Attractions
+          </div>
+          <div className="headerListItem">
+            <MdLocalTaxi />
+            Airport taxis
+          </div>
         </div>
       </div>
     </div>

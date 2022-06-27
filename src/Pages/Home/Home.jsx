dc8dd80ceeeee8f37359c1700a3111196cce22c2
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 14:41:08 +0600

    MailList add home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Featured from "../../Components/Featured/Featured";
 import FeaturedProperties from "../../Components/FeaturedProperties/FeaturedProperties";
 import Header from "../../Components/Header/Header";
+import MailList from "../../Components/MailList/MailList";
 import Navbar from "../../Components/Navbar/Navbar";
 import PropertyList from "../../Components/PropertyList/PropertyList";
 import "./Home.css";
@@ -17,6 +18,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
     </section>
   );

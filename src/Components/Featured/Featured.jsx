8e23fe65c005f96a61a20dd8c600a815a1b6c907
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 11:47:54 +0600

    featured done

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -6,6 +6,7 @@ const Featured = () => {
     <div className="featured">
       <div className="featuredItem">
         <img
+          className="featuredImg"
           src="https://t-cf.bstatic.com/xdata/images/city/540x270/957800.webp?k=6e50f4c36265ab07bf1c6498c298d8a9de40400f606365b83266a3e7ffe67877&o="
           alt="Dublin"
         />
@@ -16,6 +17,7 @@ const Featured = () => {
       </div>
       <div className="featuredItem">
         <img
+          className="featuredImg"
           src="https://t-cf.bstatic.com/xdata/images/city/540x270/690329.webp?k=330403cfa8e49ee98d01a4808f169d520e49f24348154b8cb7f637497015802f&o="
           alt="Dublin"
         />
@@ -26,6 +28,7 @@ const Featured = () => {
       </div>
       <div className="featuredItem">
         <img
+          className="featuredImg"
           src="https://t-cf.bstatic.com/xdata/images/city/540x270/689874.webp?k=5a29b0d987e962b9ce2c78ae5dae52d02fb2ed959b1e2b00faf022306e103f8b&o="
           alt="Dublin"
         />

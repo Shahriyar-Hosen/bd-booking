e0e23847a1d0142cc3bd244261c8b26cb932ac5f
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 13:26:30 +0600

    FeaturedProperties setup done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Featured from "../../Components/Featured/Featured";
+import FeaturedProperties from "../../Components/FeaturedProperties/FeaturedProperties";
 import Header from "../../Components/Header/Header";
 import Navbar from "../../Components/Navbar/Navbar";
+import PropertyList from "../../Components/PropertyList/PropertyList";
 import "./Home.css";
 
 const Home = () => {
@@ -11,6 +13,10 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </section>
   );

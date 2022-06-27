68716a1ac61e25871cc135b06b8835122e612e92
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 13:18:19 +0600

    FeaturedProperties compo add

---
+++ src/Components/FeaturedProperties/FeaturedProperties.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FeaturedProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div>
+      <h1>Featured Properties</h1>
+    </div>
+  );
+};
+
+export default FeaturedProperties;

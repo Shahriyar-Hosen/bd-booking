98149699bb9c4645af5543ca2f179aa5ccc77792
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 15:01:38 +0600

    Footer setup done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Featured from "../../Components/Featured/Featured";
 import FeaturedProperties from "../../Components/FeaturedProperties/FeaturedProperties";
+import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import MailList from "../../Components/MailList/MailList";
 import Navbar from "../../Components/Navbar/Navbar";
@@ -19,6 +20,7 @@ const Home = () => {
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
+        <Footer />
       </div>
     </section>
   );

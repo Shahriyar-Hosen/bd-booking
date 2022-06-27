0272c48915edd6bd4314ddadcbc603af7ab4a07d
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 12:31:22 +0600

    PropertyList compo add done

---
+++ src/Components/PropertyList/PropertyList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PropertyList.css";
+
+const PropertyList = () => {
+  return (
+    <div>
+      <h1>properties</h1>
+    </div>
+  );
+};
+
+export default PropertyList;

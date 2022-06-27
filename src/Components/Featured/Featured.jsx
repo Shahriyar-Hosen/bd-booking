c3e15e47f8a0d22cbc66982f8eef4da00fa053dd
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 11:25:22 +0600

    Austin & Reno setup done

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -14,6 +14,26 @@ const Featured = () => {
           <h2>123 properties</h2>
         </div>
       </div>
+      <div className="featuredItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/540x270/690329.webp?k=330403cfa8e49ee98d01a4808f169d520e49f24348154b8cb7f637497015802f&o="
+          alt="Dublin"
+        />
+        <div className="featuredTitles">
+          <h1>Austin</h1>
+          <h2>713 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/540x270/689874.webp?k=5a29b0d987e962b9ce2c78ae5dae52d02fb2ed959b1e2b00faf022306e103f8b&o="
+          alt="Dublin"
+        />
+        <div className="featuredTitles">
+          <h1>Reno</h1>
+          <h2>77 properties</h2>
+        </div>
+      </div>
     </div>
   );
 };

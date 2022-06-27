fb932126bb8b8e51c125cfd0e8258fd3322a7d17
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-27 14:20:35 +0600

    MailList setup done

--- src/Components/FeaturedProperties/FeaturedProperties.jsx
+++ src/Components/FeaturedProperties/FeaturedProperties.jsx
@@ -3,7 +3,7 @@ import "./FeaturedProperties.css";
 
 const FeaturedProperties = () => {
   return (
-    <div className="fp">
+    <section className="fp">
       <div className="fpItem">
         <img
           src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
@@ -60,7 +60,7 @@ const FeaturedProperties = () => {
           <span>Excellent</span>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/Components/MailList/MailList.jsx
+++ src/Components/MailList/MailList.jsx
@@ -3,9 +3,16 @@ import "./MailList.css";
 
 const MailList = () => {
   return (
-    <div>
-      <h1>MailList</h1>
-    </div>
+    <section className="mail">
+      <h1 className="mailTitle">Save time, save money!</h1>
+      <span className="mailDesc">
+        Sign up and we'll send the best deals to you
+      </span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="Your Email" />
+        <button>Subscribe</button>
+      </div>
+    </section>
   );
 };
 

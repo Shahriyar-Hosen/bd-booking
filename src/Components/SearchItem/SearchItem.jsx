1a736d57cefd403dbe45c560b7dcd27944028f22
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-28 12:23:49 +0600

    SearchItem compo add done

---
+++ src/Components/SearchItem/SearchItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchItem.css";
+
+const SearchItem = () => {
+  return (
+    <div>
+      <h1>SearchItem</h1>
+    </div>
+  );
+};
+
+export default SearchItem;

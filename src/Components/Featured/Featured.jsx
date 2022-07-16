e876c3935dba20b3f5162956d4a200eed6590ef8
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-16 16:44:40 +0600

    hotel data load

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch.js";
 import "./Featured.css";
 
 const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrid,london"
+  );
+  console.log(data);
+  
   return (
     <div className="featured">
       <div className="featuredItem">

e5dea8de77f14a1fd77b30eeb872a777788a0a41
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-17 09:33:54 +0600

    home page link logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import "./Navbar.css"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">BD Booking!</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">BD Booking!</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
--- src/Components/PropertyList/PropertyList.jsx
+++ src/Components/PropertyList/PropertyList.jsx
@@ -5,7 +5,7 @@ import "./PropertyList.css";
 const PropertyList = () => {
   const { data, loading, error } = useFetch("/hotels/countByType");
   console.log(error);
-  
+
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",

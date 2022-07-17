84ec7ccafff86fe27c686c7165ed3f830286f67e
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-17 08:59:51 +0600

    PropertyList data load

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -6,7 +6,7 @@ const Featured = () => {
   const { data, loading, error } = useFetch(
     "/hotels/countByCity?cities=berlin,madrid,london"
   );
-  console.log(data);
+  console.log(error);
 
   return (
     <div className="featured">

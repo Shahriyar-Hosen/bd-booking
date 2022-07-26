83f0a3357145f3a70cbc5a6c1c8b742a353f4327
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-26 10:40:40 +0600

    login setup & style done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./Pages/Authentication/Login/Login";
 import Home from "./Pages/Home/Home";
 import Hotel from "./Pages/Hotel/Hotel";
 import List from "./Pages/List/List";
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/hotels" element={<List />}></Route>
         <Route path="/hotels/:id" element={<Hotel />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  //   const { loading, error, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    // dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      //   dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      navigate("/");
+    } catch (err) {
+      //   dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+        {/* disabled={loading} */}
+        <button onClick={handleClick} className="lButton">
+          Login
+        </button>
+        {/* {error && <span>{error.message}</span>} */}
+      </div>
+    </div>
+  );
 };
 
 export default Login;

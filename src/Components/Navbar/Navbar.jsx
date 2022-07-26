898ff00418b0e214228b7fe3a77340e614c67227
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-26 18:28:01 +0600

    navaber login & register done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import { signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init.js";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [user, loading, error] = useAuthState(auth);
+  let username =
+    JSON.parse(localStorage.getItem("user")).username || "user Name";
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">BD Booking!</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          <span>
+            {username}
+            <button onClick={() => signOut(auth)} className="navButton">
+              Logout
+            </button>
+          </span>
+        ) : (
+          <div className="navItems">
+            <Link to="/register">
+              <button className="navButton">Register</button>
+            </Link>
+            <Link to="/login">
+              <button className="navButton">Login</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );

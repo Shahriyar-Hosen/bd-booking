a1f4e728484d23ab15d8ff4c4f24293a2822208a
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-30 15:45:21 +0600

    clear code data

--- src/Components/Reserve/Reserve.jsx
+++ src/Components/Reserve/Reserve.jsx
@@ -10,10 +10,12 @@ const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
   const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
   const { dates } = useContext(SearchContext);
+  const navigate = useNavigate();
 
   console.log("Reserve: ", loading);
   console.log("Reserve: ", error);
 
+  // Get Dates InRange
   const getDatesInRange = (startDate, endDate) => {
     const start = new Date(startDate);
     const end = new Date(endDate);
@@ -32,6 +34,7 @@ const Reserve = ({ setOpen, hotelId }) => {
 
   const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate);
 
+  // Is Available
   const isAvailable = (roomNumber) => {
     const isFound = roomNumber.unavailableDates.some((date) =>
       allDates.includes(new Date(date).getTime())
@@ -40,6 +43,7 @@ const Reserve = ({ setOpen, hotelId }) => {
     return !isFound;
   };
 
+  // Handle Select
   const handleSelect = (e) => {
     const checked = e.target.checked;
     const value = e.target.value;
@@ -50,8 +54,8 @@ const Reserve = ({ setOpen, hotelId }) => {
     );
   };
 
-  const navigate = useNavigate();
 
+  // Handle Click
   const handleClick = async () => {
     try {
       await Promise.all(
